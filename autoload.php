216364f211f95f945dce2eefafd46d79e763c063
andriyantopajiri2020 <72873413+andriyantopajiri2020@users.noreply.github.com>
2022-09-29 15:07:18 +0800

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$autoload['packages'] = array();
+
+
+$autoload['libraries'] = array('database', 'session', 'form_validation', 'encryption');
+
+
+$autoload['drivers'] = array();
+
+// Contoh File Helper: my_helper.php
+$autoload['helper'] = array('url', 'my_helper');
+
+
+$autoload['config'] = array();
+
+
+$autoload['language'] = array();
+
+
+$autoload['model'] = array();
